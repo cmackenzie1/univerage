7b543bc8f8f93197ed68da2ddff79e4b714ffdb0
Cole Mackenzie <colemackenzie1@gmail.com>
2019-05-28 18:03:09 -0700

    Adding term data and generation script

---
+++ scripts/terms.py
@@ -0,0 +1,1 @@
+TERMS = ["Winter", "Spring", "Summer", "Fall"]with open("terms.txt", "w") as f:    id = 1    for year in range(2000, 2020):        for term in TERMS:            f.write("({id},'{term}',{year}),\n".format(id=id, term=term, year=year))            id += 1
--- src/test/java/com/univerage/univerage/repository/TermRepositoryTest.java
+++ src/test/java/com/univerage/univerage/repository/TermRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.univerage.univerage.repository;
 
-import com.univerage.univerage.model.Instructor;
 import com.univerage.univerage.model.Term;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,10 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
