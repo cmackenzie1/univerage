f2df6a6d95db62374a227c432592fe6568511f43
Cole Mackenzie <colemackenzie1@gmail.com>
2019-06-06 20:30:55 -0700

    removing volumes

--- scripts/mongo/import.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mongoimport --db univerage --collection terms --file /import/terms.json --jsonArray
-mongoimport --db univerage --collection courses --file /import/courses.json --jsonArray
-mongoimport --db univerage --collection classes --file /import/classes.json --jsonArray
